1dfcdfeeade754e6446b5b2ebf6ffbd2a2a50aff
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-01-20 21:50:51 -0400

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,85 @@
 const path = require('path');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
+const production = process.env.NODE_ENV === 'production';
 
-module.exports = function(env, argv){
-  const plugins = [];
-  if (env.prod) {
-    plugins.push(new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }));
-  };
+const styleLoaders = [
+  { loader: 'style-loader', options: { sourceMap: true, hmr: !production } },
+  { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+  { loader: 'postcss-loader', options: { sourceMap: true } },
+];
 
-  const styleLoaders = [
-    { loader: 'style-loader', options: { sourceMap: true, hmr: env.dev } },
-    { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-    { loader: 'postcss-loader', options: { sourceMap: true } },
-  ];
+module.exports = (env, argv) => ({
+  context: __dirname, // project root directory
 
+  entry: "./src/index.js",
 
-  return {
-    context: __dirname, // project root directory
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    pathinfo: production ? false : true,
+  },
 
-    entry: "./src/index.js",
-
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-      pathinfo: env.prod ? false : true,
-    },
-
-    module: {
-      rules: [
-        {
-          resource: {
-            test: /\.jsx?$/,
-            include: [ path.resolve(__dirname, "src/") ],
-          },
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: ['env', 'react'],
-              }
+  module: {
+    rules: [
+      {
+        resource: {
+          test: /\.jsx?$/,
+          include: [ path.resolve(__dirname, "src/") ],
+        },
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react'],
             }
-          ],
+          }
+        ],
+      },
+      {
+        resource: {
+          test: /\.[sp]?css$/,
+          include: [ path.resolve(__dirname, "src/styles/") ],
         },
-        {
-          resource: {
-            test: /\.[sp]?css$/,
-            include: [ path.resolve(__dirname, "src/styles/") ],
-          },
-          use: env.prod ? ExtractTextWebpackPlugin.extract({
-            fallback: styleLoaders[0],
-            use: styleLoaders.slice(1),
-          }) : styleLoaders,
-        },
-      ],
-    },
+        use: production ? ExtractTextWebpackPlugin.extract({
+          fallback: styleLoaders[0],
+          use: styleLoaders.slice(1),
+        }) : styleLoaders,
+      },
+    ],
+  },
 
-    resolve: {
-      modules: [
-        "node_modules",
-        path.resolve(__dirname, "src"),
-      ],
-      enforceExtension: true,
-      alias: {},
-    },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src"),
+    ],
+    enforceExtension: true,
+    alias: {},
+  },
 
-    plugins: env.prod ? plugins.concat([
-      new ExtractTextWebpackPlugin("styles.css"),
-    ]) : plugins.concat([]),
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new ExtractTextWebpackPlugin("styles.css"),
+  ],
 
-    devtool: env.prod ? "source-map" : "eval-source-map", // enum
+  devtool: production ? "source-map" : "eval-source-map", // enum
 
-    devServer: {
-      contentBase: path.resolve(__dirname, "dist"),
-      port: 9000,
-      historyApiFallback: true,
-      host: "0.0.0.0",
-      open: true
-    },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
+    historyApiFallback: true,
+    host: "0.0.0.0",
+    open: true
+  },
 
-    target: "web",
+  target: "web",
 
-    performance: {
-      hints: env.prod ?  "error" : "warning", // enum
-    },
+  performance: {
+    hints: production ?  "error" : "warning", // enum
+  },
 
-    stats: "normal",
-  }
-}
+  stats: "normal",
+});
