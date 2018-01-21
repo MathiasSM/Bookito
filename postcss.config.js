ad37291039c8fa0f796d155281c5b316f3421aec
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-01-21 00:35:21 -0400

    Added postcss config (quite complex...)

---
+++ postcss.config.js
@@ -0,0 +1,28 @@
+/* eslint quote-props: 0 */
+module.exports = ({ env }) => ({
+  plugins: {
+    'postcss-easy-import': { prefix: '_' },
+    'postcss-nested-props': {},
+    'postcss-nested-vars': {},
+    'postcss-mixins': {},
+    'postcss-nested': {},
+    'postcss-current-selector': {},
+    'postcss-font-magician': {
+      display: 'swap',
+      variants: {
+        'Open Sans': {
+          '300': ['woff2'],
+          '400 italic': ['woff2'],
+        },
+      },
+    },
+    'postcss-focus': {},
+    'postcss-size': {},
+    'postcss-color-function': {},
+    'css-colorguard': {},
+    'postcss-flexbugs-fixes': {},
+    autoprefixer: { env },
+    cssnano: env === 'production' ? { preset: 'default' } : false,
+    'postcss-reporter': {},
+  },
+});
